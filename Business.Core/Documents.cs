d10cf64d7e2f3abd5a13e6a1e1ae7d135489fa3a
Nicola Iarocci <nicola@nicolaiarocci.com>
2016-05-12 18:14:59 +0200

    first commit

---
+++ Business.Core/Documents.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Amica.vNext.Models.Documents;
+using Amica.vNext;
+using System.Threading.Tasks;
+
+namespace Amica.vNext.Business
+{
+    public class Documents
+    {
+		public static async Task<DocumentNumber> GetNextNumber(Document document)
+        {
+            var number = (BusinessLogic.Cache != null && document.Category != null && document.Number != null) ?
+                await BusinessLogic.Cache?.Get<DocumentNumber>(DocumentNumberCacheKey(document)) :
+                (document.Number != null) ?
+                    document.Number :
+                    new DocumentNumber();
+            number.Numeric += 1;
+
+			if (BusinessLogic.Cache != null)
+				await BusinessLogic.Cache.Insert(DocumentNumberCacheKey(document), number);
+            return number;
+        }
+		private static string DocumentNumberCacheKey(Document document)
+        {
+            return $"{(int)document.Category.Code}/{document.Number.String.ToString()}";
+        }
+
+    }
+}
---
+++ Business.Tests/DocumentsTest.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Amica.vNext;
+using Amica.vNext.Business;
+using Amica.vNext.Models;
+using Amica.vNext.Models.Documents;
+using NUnit.Framework;
+
+namespace Business.Tests
+{
+	[TestFixture]
+    public class DocumentsTest
+    {
+		private readonly SqliteObjectCache cache = new SqliteObjectCache { ApplicationName = "test"};
+
+		[SetUp]
+		public void Setup()
+        {
+            DefaultFactories.Bootstrap();
+        }
+
+		[Test]
+		public async Task GetNextNumberNoCache()
+        {
+			// a just created document will have its DocumentNumber.Numeric property set to 1
+            var doc = Factory<Document>.Create();
+            doc.Number = await Documents.GetNextNumber(doc);
+            Assert.That(doc.Number.Numeric, Is.EqualTo(1));
+
+			// 1 again as there's no cache storing previous value
+            doc = Factory<Document>.Create();
+            doc.Number = await Documents.GetNextNumber(doc);
+            Assert.That(doc.Number.Numeric, Is.EqualTo(1));
+
+			// since there's no cache available, if document has a number already, it will simply be increased by 1
+            doc.Number.Numeric = 10;
+            doc.Number = await Documents.GetNextNumber(doc);
+            Assert.That(doc.Number.Numeric, Is.EqualTo(11));
+
+			// same behavior as above if there's a String part, as we cannot retrieve the previous value from the cache
+            doc.Number.String = "A";
+            doc.Number = await Documents.GetNextNumber(doc);
+            Assert.That(doc.Number.Numeric, Is.EqualTo(12));
+            Assert.That(doc.Number.String, Is.EqualTo("A"));
+        }
+		[Test]
+		public async Task GetNextNumberCached()
+        {
+            BusinessLogic.Cache = cache;
+
+			// a just created document will have its DocumentNumber.Numeric property set to 1
+            var doc = Factory<Document>.Create();
+            doc.Number = await Documents.GetNextNumber(doc);
+            Assert.That(doc.Number.Numeric, Is.EqualTo(1));
+
+			// 2 thist ime as there's a cache storing previous value
+            doc = Factory<Document>.Create();
+            doc.Number = await Documents.GetNextNumber(doc);
+            Assert.That(doc.Number.Numeric, Is.EqualTo(2));
+
+			//// since there's no cache available, if document has a number already, it will simply be increased by 1
+   //         doc.Number.Numeric = 10;
+   //         doc.Number = await Documents.GetNextNumber(doc);
+   //         Assert.That(doc.Number.Numeric, Is.EqualTo(11));
+
+			//// same behavior as above if there's a String part, as we cannot retrieve the previous value from the cache
+   //         doc.Number.String = "A";
+   //         doc.Number = await Documents.GetNextNumber(doc);
+   //         Assert.That(doc.Number.Numeric, Is.EqualTo(12));
+   //         Assert.That(doc.Number.String, Is.EqualTo("A"));
+        }
+    }
+}
